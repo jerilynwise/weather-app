8b490a59993a1d1cb1a2388643537d857999ad3b
jerilyn.wise@paddywax.com <jerilyn.wise@paddywax.com>
2022-06-09 10:23:24 -0500

    different direction

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+//when the search button is clicked search is hidden and the api request goes out
+
+//api pulls the city,temp,wind, humidity, and uv index for current date (0)
+
+//api then shows days (1-5) below in small tabs showing the date 
