96ea8c765867875a2c2c5ae8d97929150340df57
jerilyn.wise@paddywax.com <jerilyn.wise@paddywax.com>
2022-06-12 13:53:52 -0500

    pull coors and render data

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,51 @@
-//when the search button is clicked search is hidden and the api request goes out
+// Set global variables, including Open Weather Maps API Key
+var keyAPI = "75c11b98c1e96b59417961288d3f315d";
+var BASE_URL = "https://api.openweathermap.org"
+var currentCity = "";
+var lastCity = [];
+var searchForm = document.getElementById("searchForm")
+var searchCity = document.getElementById("searchCity");
+var current = document.getElementById("current");
 
-//api pulls the city,temp,wind, humidity, and uv index for current date (0)
 
-//api then shows days (1-5) below in small tabs showing the date 
+// CALL TO GET THE COORDINATES
+function getCoors(e) {
+    e.preventDefault()
+    var city = searchCity.value
+    var API_URL = BASE_URL + "/geo/1.0/direct?q=" + city + "&limit=5&appid=" + keyAPI
+    fetch(API_URL).then(function(res){
+        return res.json()
+    })
+    .then(function(data){
+        if (!data[0]) {
+            alert("Location Not Found")
+        }
+        else{
+            // console.log(data)
+            // function to take care of local storage
+            // addToHistory()
+            getWeather(data[0])
+        }
+    })
+}
+
+function getWeather(data) {
+    var lat = data.lat
+    var lon = data.lon
+    var city = data.name
+    var API_URL = BASE_URL + "/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=" + keyAPI
+    fetch(API_URL).then(function(res){
+        return res.json()
+    })
+    .then(function(data){
+        console.log(data)
+        // renderCurrent(city,data)
+    })
+}
+
+
+
+
+
+
+searchForm.addEventListener("submit", getCoors)
